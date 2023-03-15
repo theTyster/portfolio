2f24056dc8e052c68475a916fdd635444a8d6e58
Ty <noreply@theducksays.click>
2023-03-14 22:15:20 -0700

    Optimized and serialized the start of the story a little better. Rounding out the beginning portion a little more. Just wanting to push this commit forward so I don't lose any progress.
     Changes to be committed:
    	modified:   css/styles.css
    	modified:   js/main.js
    	modified:   page1.html

--- js/main.js
+++ js/main.js
@@ -5,20 +5,23 @@ async function storyStart(){
 	const phaseOne_hmm = new displayFunc("p.storyStart_hm"),
 		phaseOne_no = new displayFunc("p.storyStart_no"),
 		phaseOne_yes = new displayFunc("p.storyStart_yes"),
-		regex = /#f/gui,
+		regex = /#/gui,
 		colorizeDuck = function saveDuckColorToObject() {
-	//Colors the Duck based on the input.
 		page.duckInput.style.backgroundColor = page.duckInput.value;
 		duck.color = page.duckInput.value;
+		if (duck.color === "paleTurquoise")
+			duck.bonusLevel = true;
 		//saves input to memory.
 		localStorage.setItem("duck", JSON.stringify(duck));
-		return duck.color;
+		return duck;
 		}
+
 	phaseOne_hmm.show(); 
 	await sleep (1500);
 	phaseOne_hmm.hide();
 	await sleep (700);
 
+	//Colors the Duck based on the input.
 	colorizeDuck(); 
 	if (!page.duckInput.style.backgroundColor){
 		phaseOne_no.show();
@@ -32,12 +35,55 @@ async function storyStart(){
 	}
 
 	// verifies the input. If the transition occurred, then transitionend will detect it.
-	page.duckInput.addEventListener("transitionend", function storyStartListener(){
+	page.duckInput.addEventListener("transitionend", async function storyStartListener(){
+		document.removeEventListener("keyup", storyStartListener);
 		page.duckInput.disabled = true;
-		phaseOne_yes.tag.style.position = "relative";
+		phaseOne_yes.tag.style.position = "relative"; //necessary so that text doesn't get mashed. This is not important enough to add to CSS directly imo.
 		phaseOne_yes.tag.innerText = `Ah, yes! That's it. He was definitely a ${duck.color} duck.`;
 		phaseOne_yes.show();
-		document.removeEventListener("keyup", storyStartListener);
+
+
+		const phaseTwo = new displayFunc("div.phaseTwo");
+		let eyes = [document.querySelector("pre#phTwo_eyes1"), document.querySelector("pre#phTwo_eyes2")]
+
+		await sleep(2000)
+		phaseTwo.show();
+
+		for (let i of eyes){
+			i.style.textShadow = "1px 1px 2px black";
+			i.style.visibility = "collapse";
+		}
+		await sleep(3500);
+
+		for (let i of page.container)
+		i.style.flex = 1;
+		page.body.style.background = "paleTurquoise";
+
+		if (duck.bonusLevel){
+			page.body.style.filter = "invert(100%)";
+			page.body.style.background = "MidnightBlue";
+			for (let i of eyes)
+				i.style.filter = "invert(100%)";
+
+		}
+
+		eyes[0].style.visibility = "unset";
+		eyes[0].style.fontSize = "30pt";
+		await sleep(3000);
+
+		eyes[0].style.visibility = "collapse";
+		await sleep(3000);
+
+		eyes[1].style.visibility = "unset";
+		eyes[1].style.fontSize = "30pt";
+		await sleep(3000);
+
+		for (let i of eyes)
+		i.style.display = "none";
+
+		await sleep(2000);
+		const phaseThree = new displayFunc("div.phaseThree");
+		await phaseThree.show();
 	});
 }
 
@@ -48,34 +94,4 @@ document.addEventListener("keyup", function storyStartListener(event){
 	if (keyName === "Enter") {
 		storyStart();
 	};
-});
-
-
-page.duckInput.addEventListener("transitionend", function phaseTwoListener(){
-	const phaseTwo = new displayFunc("div.phaseTwo");
-	let eyes = [document.querySelector("pre#phTwo_pt2"), document.querySelector("pre#phTwo_pt3")]
-	page.body.style.backgroundColor = "#888"
-	setTimeout(() => {
-		phaseTwo.show();
-		for (let i of eyes)
-			i.style.visibility = "collapse";
-	for (let i of page.container)
-	i.style.flex = 1;
-	}, 1000);
-	page.container[0].addEventListener("transitionrun", () => {
-		eyes[0].style.visibility = "unset";
-		eyes[0].style.fontSize = "30pt";
-		setTimeout(() => {
-		eyes[0].style.visibility = "collapse";
-		}, 3000);
-		setTimeout(() => {
-			eyes[1].style.visibility = "unset";
-			eyes[1].style.fontSize = "30pt";
-		}, 3000);
-		setTimeout(() => {
-		eyes[1].style.visibility = "collapse";
-		}, 6000);
-	makeItRain(33);
-	});
-	document.removeEventListener("transitionend", phaseTwoListener);
 });
