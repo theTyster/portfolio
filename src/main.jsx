6e67c0b0b1ac375d3f4f3d61fcce249bfa67a0c7
T <noreply@theducksays.click>
2023-07-19 12:05:47 -0700

    I have succesfully integrated react into this project.

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+// Generates the Header and Footer of each page.
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a rel="noreferrer noopener" target="_blank" href="https://tydavisportfolio.wordpress.com">About The Author</a></li>
+        <li><a rel="noreferrer noopener" target="_blank" href="https://github.com/theTyster/duck_story">The Code</a></li>
+        <li><a href="page1.html">Start The Story</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+const header = ReactDOM.createRoot(document.querySelector('header'));
+const footer = ReactDOM.createRoot(document.querySelector('footer'));
+header.render(<Navigation />);
+footer.render(<Navigation />);
