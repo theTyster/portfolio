0cedba790b980e5c0fe21d9b36c44592a6047923
T <noreply@theducksays.click>
2023-07-24 18:03:52 -0700

    Added a hamburger menu and footer with React.

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,55 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './nav.css'
 
-// Generates the Header and Footer of each page.
-function Navigation() {
+
+// Generates the Header and Footer.
+function NavButton() {
+  const [menuState, menuSet] = React.useState(false);
+  const onClick = () => menuState ? menuSet(false) : menuSet(true);
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a rel="noreferrer noopener" target="_blank" href="https://tydavisportfolio.wordpress.com">About The Author</a></li>
-        <li><a rel="noreferrer noopener" target="_blank" href="https://github.com/theTyster/duck_story">The Code</a></li>
-        <li><a href="page1.html">Start The Story</a></li>
-      </ul>
-    </nav>
+    <>
+      <button className='menuHam' onClick={ onClick }> 
+          <svg width="16px" height="16px" version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" stroke="#6c623a" strokeLinecap="round" strokeWidth="1">
+              <path d="m0 4 h16"/>
+              <path d="m0 8 h16"/>
+              <path d="m0 12 h16"/>
+            </g>
+          </svg>
+      </button>
+      { menuState ? <Menu /> : null }
+    </>
   );
 }
 
+
+function Menu() {
+  return (
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a rel="noreferrer noopener" target="_blank" href="https://tydavisportfolio.wordpress.com">About The Author</a></li>
+          <li><a rel="noreferrer noopener" target="_blank" href="https://github.com/theTyster/duck_story">The Code</a></li>
+          <li><a href="page1.html">Start The Story</a></li>
+        </ul>
+      </nav>
+  );
+}
+
+
+function Credit() {
+  return (
+    <p>A story by Ty ©️ {new Date().getFullYear()}</p>
+  );
+}
+
+
 const header = ReactDOM.createRoot(document.querySelector('header'));
+header.render(<NavButton />);
+
 const footer = ReactDOM.createRoot(document.querySelector('footer'));
-header.render(<Navigation />);
-footer.render(<Navigation />);
+footer.render(<Credit />)
+
+
+
+
