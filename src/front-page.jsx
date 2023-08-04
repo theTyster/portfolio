7003285d61a901161b5bfb201f5ef10c93597a5a
T <noreply@theducksays.click>
2023-08-03 23:41:44 -0700

    Added animations for the navbutton and the duck. The nav button was tricky.

--- src/front-page.jsx
+++ src/front-page.jsx
@@ -6,10 +6,20 @@ import './css/duck-svg.scss';
 import { ReactComponent as DuckSVG } from './front-page/img/duck.svg';
 import Text from './front-page/text.jsx';
 
-const onClick = () => {
+const onClick = async () => {
+	const duckTl = gsap.timeline();
+	
 
-	gsap.to("#head", {rotate: 13, transformOrigin: "50% 50%"});
-	gsap.to("#r_wing", {rotate: 130, transformOrigin: "50% 15%"});
+	await duckTl
+		.to("#head", {duration: .4, ease:"back", rotate: 13, transformOrigin: "50% 50%"})
+		.to("#lt_eyelid", {duration:.2, ease:"back",  yoyo:true, repeat:1, y:"+=10", x:"+=2.7"},.5)
+		.to("#rt_eyelid", {duration:.2, ease:"back",  yoyo:true, repeat:1, y:"+=10", x:"+=4"},.5)
+		.to("#lb_eyelid", {duration:.2, ease:"back",  yoyo:true, repeat:1, y:"-=5", x:"-=2.5"},.5)
+		.to("#rb_eyelid", {duration:.2, ease:"back",  yoyo:true, repeat:1, y:"-=5", x:"-=2.5"},.5)
+		.to("#r_wing", {duration:.05, yoyo:true, repeat:3, rotate:130, transformOrigin:"50% 15%"},1)
+		.to("#l_wing", {duration:.05, yoyo:true, repeat:3, rotate:50, transformOrigin:"50% 15%"},1);
+
+	window.open("/page1.html", "_self");
 
 }
 
--- src/utils/nav.jsx
+++ src/utils/nav.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Transition } from 'react-transition-group';
+import gsap from 'gsap';
 import PropTypes from 'prop-types';
 
-// Generates the Header and Footer.
 function NavButton({ onClick, menuState }) {
 
   // PROPS VALIDATION
@@ -15,10 +15,10 @@ function NavButton({ onClick, menuState }) {
     <>
       <button className='menuHam' onClick={ onClick }> 
           <svg width="16px" height="16px" version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" stroke="#6c623a" strokeLinecap="round" strokeWidth="1">
-              <path d="m0 4 h16"/>
-              <path d="m0 8 h16"/>
-              <path d="m0 12 h16"/>
+            <g id="ham" fill="none" stroke="#6c623a" strokeLinecap="round" strokeWidth="2">
+              <path id="ham-top" d="m1 4 h14"/>
+              <path id="ham-middle" d="m1 8 h14"/>
+              <path id="ham-bottom" d="m1 12 h14"/>
             </g>
           </svg>
       </button>
@@ -44,7 +44,7 @@ function Menu({ menuState }) {
       maxHeight: '236px',
     },
     entered: {
-      visibility: 'isible',
+      visibility: 'visible',
       maxHeight: '236px',
     },
     exiting: {
@@ -78,11 +78,24 @@ function Menu({ menuState }) {
 function Navigation() {
 
   const [menuState, menuSet] = React.useState(false);
-  const onClick = () => menuSet(!menuState);
+  const [hamTl] = React.useState(gsap.timeline()); // used to trigger the render.
+  const ham = {};
+
+  React.useEffect(() => {
+    hamTl // Every time the state is changed useEffect re-renders a new timeline and plays this.
+      .to("#ham-top", {duration: 0.4, y: 4})
+      .to("#ham-bottom", {duration: 0.4, y: -4},"<")
+      .reverse();
+  }, [])
+
+  const onClick = () => {
+    hamTl.reversed(!hamTl.reversed()); //toggles the direction of the playhead after every render.
+    menuSet(!menuState);
+  }
 
 
   return(
-    <NavButton menuState={ menuState } onClick={ onClick } />
+    <NavButton ham={ ham } menuState={ menuState } onClick={ onClick } />
   );
 }
 
