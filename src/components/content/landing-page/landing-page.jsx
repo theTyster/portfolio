7f16b740975c8748c6909a615011351b41fa5212
thetyster <noreply@theducksays.click>
2024-02-29 17:30:24 -0800

    Updates nav CSS.

--- src/components/content/landing-page/landing-page.jsx
+++ src/components/content/landing-page/landing-page.jsx
@@ -1,23 +1,36 @@
-//DEV Libraries
-import {React} from "react";
+//DEV LIBRARIES
+import react from "react";
 
-//CSS TODO
-//import "./css/front-page.scss";
+//CSS
+import "../../../assets/css/landing-page.scss";
 
-function FrontPage(){
+//COMPONENTS
+import Showcase from "./showcase.jsx";
+
+function LandingPage(){
 
   return(
     <>
-      <h1>My values in a website</h1>
+      <h1>Tyler Davis</h1>
+      <img className="my-headshot" src="img/me.jpg" alt="My Face." />
+      <hr />
+      <p>I like to keep things, lean, fast, simple and usable. Accessibility trumps any other purpose the web could have.</p>
+      <blockquote>
+      <p>
+        <i>&quot;Web pages are designed for people.&quot;</i>
+      </p>
+       <p>-Tim Berners-Lee</p>
+      </blockquote>
+      <h2>Some things that matter to me...</h2>
       <ul>
-        <li>Fast</li>
-        <li>Lightweight</li>
-        <li>Simple</li>
-        <li>Transparent</li>
-        <li>Scalable</li>
+        <li>Accessibility</li>
+        <li>Speed</li>
+        <li>Transparency</li>
+        <li>User Empowerment</li>
       </ul>
+      <Showcase />
     </>
   )
 }
 
-export default FrontPage;
+export default LandingPage;
