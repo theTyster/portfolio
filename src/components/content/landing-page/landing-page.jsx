67d041fcabf938b9d92cc828cfaa494fdf7aeb81
thetyster <noreply@mail.thetyster.dev>
2024-03-26 13:18:15 -0700

    Fix: Retry github API if no data is retrieved.

--- src/components/content/landing-page/landing-page.jsx
+++ src/components/content/landing-page/landing-page.jsx
@@ -35,21 +35,29 @@ function LandingPage(){
 
   // PULL CONTRIBUTIONS DATA
   const githubApiSearch = "https://api.github.com/search/issues?q=author%3Athetyster+type%3Apr";
-  const githubHTMLSearch = "https://github.com/search?q=author%3Athetyster+type%3Apr";
   let [pullRequests, setPullRequests] = useState();
   let [orgs, setOrgs] = useState();
 
   useEffect(()=> {(async ()=>{
 
     // GET PULL REQUEST DATA
-    const resp = await axios(githubApiSearch);
-    const search = resp.data.items
+    let search;
+    while(!search){
+      try {
+        const resp = await axios(githubApiSearch);
+        search = resp.data.items
+      }
+      catch (error){
+        console.log("API Error. Trying Again. ", error);
+        await sleep(120);
+      }
+    }
 
     // FILTER OUT MY OWN PULL REQUESTS.
     const pullRequests = search.filter((pr)=>
       pr.author_association === "CONTRIBUTOR"&&
-      pr.pull_request.merged_at && 
-      pr.state === "closed" || 
+      pr.pull_request.merged_at &&
+      pr.state === "closed" ||
       pr.state === "open"
     )
 
@@ -59,61 +67,72 @@ function LandingPage(){
 
     // GET ASSOCIATED DATA OF ORGS
     let orgData = {};
-    await axios.all(
-      prOrgsUrls.map(url => axios(url))
-    )
-    // move data into an object with the url set as the key for each org.
-      .then(resps => [...resps].map(resp => 
-        orgData[resp.data.url] = resp.data)
-      )
+    while(Object.keys(orgData).length === 0){
+      try{
+        await axios.all(
+          prOrgsUrls.map(url => axios(url))
+        )
+        // move data into an object with the url set as the key for each org.
+          .then(resps => [...resps].map(resp =>
+            orgData[resp.data.url] = resp.data)
+          )
+      }
+      catch (error){
+        console.log("API Error. Trying Again. ", error);
+        await sleep(120);
+      }
+    }
 
     setPullRequests(pullRequests);
     setOrgs(orgData);
   })()}, []);
 
+  // TAB MENU DATA
   const menuItems = new Map([
     [
       {
         "job-history": 0,
-        "my-work": 1,
+        "initial": 1,
         "contributions": 2,
         "hobbies": 3,
       },
       [
         {
     //0
+        id: "job-history",
         component: <JobHistory />,
         title: "Job History",
-        id:"job-history"
         },
         {
     //1
+        id: "initial",
         component: <MyWork />,
         title: "My Work",
-        id:"my-work"
         },
         {
     //2
-        component: <Contributions 
-          orgs={orgs} 
-          pullRequests={pullRequests}
+        id: "contributions",
+        component:
+          <Contributions
+            orgs={orgs}
+            pullRequests={pullRequests}
           />,
         title: "Contributions",
-        id:"contributions"
         },
         {
     //3
+        id: "hobbies",
         component: <Hobbies />,
         title: "Hobbies",
-        id:"hobbies"
         }
       ]
     ]
   ])
 
+
   return(
-    <>
-      <AttetionGetterImage 
+    <div className="landing-page">
+      <AttetionGetterImage
         imgClass="my-headshot"
         sideText_classPrefix="headshot-text"
         imgSrc="img/me.jpg"
@@ -123,9 +142,10 @@ function LandingPage(){
 
       <hr />
 
-    <TabMenu menuItems={menuItems} />
-
-    </>
+      <TabMenu
+        menuItems={menuItems}
+      />
+    </div>
   )
 }
 
