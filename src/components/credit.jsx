d70b3d5a0834802aafa46af5911b16035f6866d3
thetyster <noreply@mail.thetyster.dev>
2024-04-02 18:16:10 -0700

    Fix: Missing semicolon.

--- src/components/credit.jsx
+++ src/components/credit.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 
-const Credit = () => <p>Ty Davis ©️ {new Date().getFullYear()}</p>
+const Credit = () => <p>Ty Davis ©️ {new Date().getFullYear()}</p>;;
 
 export default Credit;
