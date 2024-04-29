34c023e680b93c1126e0ff2d3fd37256288be895
thetyster <noreply@mail.thetyster.dev>
2024-04-29 12:30:27 -0700

    Feat: Adds a 404 page.

---
+++ site/src/404/404.jsx
@@ -0,0 +1,49 @@
+//DEV Libraries
+import ReactDOM from "react-dom/client";
+import {React, StrictMode} from "react";
+import {setTitle} from "@utils/utils.js";
+
+//COMPONENTS
+import Navigation from "@components/navigation/nav.jsx";
+import Credit from "@components/credit/credit.jsx";
+import SvgLost from "@img/lost-svg.jsx";
+
+//CSS
+import "./404.scss";
+
+const Content = () => {
+
+  setTitle("Lost?");
+
+  return(
+    <main>
+			<h2>That doesn&apos;t seem right...</h2>
+			<SvgLost />
+			<p>Try heading back to the home page.</p>
+			<a className="button" href="/">Go Home</a>
+    </main>
+  )
+}
+
+
+// RENDERS
+const nav = ReactDOM.createRoot(document.getElementById("nav"));
+nav.render(
+  <StrictMode>
+  <Navigation />
+  </StrictMode>
+)
+
+const content = ReactDOM.createRoot(document.getElementById("content"));
+content.render(
+  <StrictMode>
+  <Content />
+  </StrictMode>
+);
+
+const credit = ReactDOM.createRoot(document.getElementById("credit"));
+credit.render(
+  <StrictMode>
+  <Credit />
+  </StrictMode>
+);
