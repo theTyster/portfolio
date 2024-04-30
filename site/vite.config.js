65af046c39b215572ec38d3bf50e7f83035040a6
thetyster <noreply@mail.thetyster.dev>
2024-04-30 10:13:53 -0700

    Fix: Adds 404 page to build tool correctly.

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,22 +4,23 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
-	root: 'site',
+  root: 'site',
   plugins: [react()],
-	build :{ 
-		rollupOptions: {
-			input: {
-				app: fileURLToPath(new URL('index.html', import.meta.url)),
-				jeopardy: fileURLToPath(new URL('my-work/jeopardy/index.html', import.meta.url)),
-			},
-		}
-	},
-	resolve: {
-		alias: [
-			{ find: "@components", replacement: fileURLToPath(new URL('src/assets/components', import.meta.url)) },
-			{ find: "@utils", replacement: fileURLToPath(new URL('src/assets/utils', import.meta.url)) },
-			{ find: "@img", replacement: fileURLToPath(new URL('src/assets/img', import.meta.url)) },
-			{ find: "@css", replacement: fileURLToPath(new URL('src/assets/css', import.meta.url)) },
-		]
-	}
+  build :{
+    rollupOptions: {
+      input: {
+        app: fileURLToPath(new URL('index.html', import.meta.url)),
+        jeopardy: fileURLToPath(new URL('my-work/jeopardy/index.html', import.meta.url)),
+        404: fileURLToPath(new URL('404.html', import.meta.url)),
+      },
+    }
+  },
+  resolve: {
+    alias: [
+      { find: "@components", replacement: fileURLToPath(new URL('src/assets/components', import.meta.url)) },
+      { find: "@utils", replacement: fileURLToPath(new URL('src/assets/utils', import.meta.url)) },
+      { find: "@img", replacement: fileURLToPath(new URL('src/assets/img', import.meta.url)) },
+      { find: "@css", replacement: fileURLToPath(new URL('src/assets/css', import.meta.url)) },
+    ]
+  }
 });
