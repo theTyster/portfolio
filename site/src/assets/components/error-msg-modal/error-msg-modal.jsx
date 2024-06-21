bb9fe707d6e55355e05b25d8a0e16974672ab8bc
thetyster <noreply@mail.thetyster.dev>
2024-06-21 09:14:45 -0700

    Refactor: With prettier.

--- site/src/assets/components/error-msg-modal/error-msg-modal.jsx
+++ site/src/assets/components/error-msg-modal/error-msg-modal.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import PropTypes from "prop-types";
 import NewTabLink from "@components/safe-link/new-tab-link";
 
@@ -8,49 +8,39 @@ import SvgExitButton from "@img/exit-button-svg.jsx";
 //CSS
 import "./error-msg-modal.scss";
 
-const ErrorMessageModal = ({
-  msg,
-  imgSrc,
-  imgAlt,
-  buttonLink,
-  buttonText,
-})=>{
-
+const ErrorMessageModal = ({ msg, imgSrc, imgAlt, buttonLink, buttonText }) => {
   //Props
-  ErrorMessageModal.propTypes= {
+  ErrorMessageModal.propTypes = {
     msg: PropTypes.string.isRequired,
     imgSrc: PropTypes.string,
     imgAlt: PropTypes.string,
     buttonLink: PropTypes.string,
     buttonText: PropTypes.string,
-  }
+  };
 
   const [isClosed, setClosed] = useState(false);
   const handleClick = () => setClosed(true);
 
-  return !isClosed? (
+  return !isClosed ? (
     <div className="error-msg">
-      <button
-        className="error-message-exit"
-        onClick={handleClick}
-      >
-        <SvgExitButton
-          className="error-message-exit"
-        />
+      <button className="error-message-exit" onClick={handleClick}>
+        <SvgExitButton className="error-message-exit" />
       </button>
       <h3>Whoops!</h3>
-      {(()=>{
-        if(imgSrc) return <img src={imgSrc} alt={imgAlt} />
+      {(() => {
+        if (imgSrc) return <img src={imgSrc} alt={imgAlt} />;
       })()}
       <p>{msg}</p>
-      {(()=>{
-        if(buttonLink) return <NewTabLink link={buttonLink}><button>{buttonText}</button></NewTabLink>
+      {(() => {
+        if (buttonLink)
+          return (
+            <NewTabLink link={buttonLink}>
+              <button>{buttonText}</button>
+            </NewTabLink>
+          );
       })()}
     </div>
-  )
-  :
-  undefined;
-}
-
+  ) : undefined;
+};
 
 export default ErrorMessageModal;
